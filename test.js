a49bdc3d773ec808c37ce7be76c0d085daa55e17
mwhitman <mwhitman95@gmail.com>
2021-11-02 11:21:26 -0400

    remove test

--- test.js
+++
@@ -1,9 +0,0 @@
-const gitanaJson = require("./gitana.json");
-const cloudcms = require(".");
-
-(async function test() {
-
-    const session = await cloudcms.connect(gitanaJson);
-
-    const branch = await session.readBranch("026e69ddefe3a5a6a6cc", "master");
-})();
